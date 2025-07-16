6212e755b93335fc155dcd6ed8c6addb9c09092c
Milton <milton.savander@gmail.com>
2025-07-16 16:34:04 +0200

    styled - styled basic dark and light themes

---
+++ src/components/WeatherDashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Location from "@/components/Location";
+
+function WeatherDashboard() {
+  return (
+    <main className="w-7xl h-[600px] bg-card rounded-2xl p-8 m-2">
+      <Location />
+    </main>
+  );
+}
+
+export default WeatherDashboard;
